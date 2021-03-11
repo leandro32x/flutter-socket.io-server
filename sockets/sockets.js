7bc34c6353e14ea2a88479d17bfd6cf45640b54e
Hector Leandro Paula <leo32x@gmail.com>
2021-03-10 21:15:03 -0400

    Server finished

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,7 +1,22 @@
 const { io } = require('../index');
+const Bands = require('../models/Bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Linkin Park'));
+bands.addBand(new Band('Molotov'));
+bands.addBand(new Band('Nirvana'));
+bands.addBand(new Band('System of a Down'));
+
+console.log(bands);
+
 //Mensajes del socket
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
     
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
@@ -12,6 +27,30 @@ io.on('connection', client => {
     client.on('mensaje', ( payload )=>{
         console.log('Mensaje recibido!!', payload);
         io.emit('mensaje',{hello:'Desde mi backed hacia todos'});
-    })
+    });
+
+    client.on('emitir-mensaje', (payload)=>{
+        console.log('mensaje nuevo!', payload);
+        io.emit('nuevo-mensaje', payload);
+    });
+
+    client.on('vote-band', (payload)=>{
+        bands.voteBand(payload.id);
+        console.log(payload);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload)=>{
+        bands.addBand(new Band(payload.name));
+        console.log(payload.name)
+        io.emit('active-bands', bands.getBands());
+        
+    });
+
+    client.on('delete-band',(payload)=>{
+        console.log(payload.id);
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
 
   });
